7adcffc6955b1c4844affcf3b23c588347a84eb6
hhhan0315 <hhan0315@naver.com>
2022-12-29 15:22:20 +0900

    <feat> SearchView 구현

--- Unsplash_SwiftUI/View/SearchView.swift
+++ Unsplash_SwiftUI/View/SearchView.swift
@@ -11,79 +11,72 @@ import Combine
 struct SearchView: View {
     @State private var photos: [Photo] = []
     @State private var searchText = ""
+    @State private var page = 0
+    @State private var isLoading = false
     @State private var cancellable: AnyCancellable?
     
-    @State private var columns: Int = 2
-    
-    var body: some View {        
+    var body: some View {
         NavigationView {
-            StaggeredGrid(columns: columns, list: photos, content: { photo in
-                ImageView(url: photo.urls.regular)
-            })
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        columns += 1
-                    } label: {
-                        Image(systemName: "plus")
+            ScrollView {
+                LazyVStack(spacing: 10) {
+                    ForEach(photos, id: \.self) { photo in
+                        NavigationLink {
+                            PhotoDetailView(photo: photo)
+                        } label: {
+                            ImageView(url: photo.urls.regular)
+                        }
+                        .buttonStyle(.plain)
                     }
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        columns = max(columns - 1, 1)
-                    } label: {
-                        Image(systemName: "minus")
-                    }
+                    
+                    ProgressView()
+                        .frame(width: UIScreen.main.bounds.width)
+                        .progressViewStyle(.circular)
+                        .onAppear {
+                            fetchSearchPhotos()
+                        }
                 }
             }
-            .animation(.easeInOut, value: columns)
-            .padding(.horizontal)
+            .listStyle(.plain)
             .navigationTitle("Search")
             .navigationBarTitleDisplayMode(.inline)
+            .padding(.horizontal)
         }
         .searchable(
             text: $searchText,
-            placement: .navigationBarDrawer(displayMode: .always)
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: Text("Search Photos...")
         )
         .onSubmit(of: .search) {
-            print(searchText)
+            self.photos.removeAll()
+            self.page = 0
+            fetchSearchPhotos()
         }
     }
     
-//    private func fetchPhotos() {
-//        let photoRequestDTO = PhotoRequestDTO(page: 1)
-//        
-//        guard let urlRequest = API.getListPhotos(photoRequestDTO).urlRequest else {
-//            return
-//        }
-//        
-//        cancellable = URLSession.shared.dataTaskPublisher(for: urlRequest)
-//            .map { $0.data }
-//            .decode(type: [PhotoResponseDTO].self, decoder: JSONDecoder())
-//            .replaceError(with: [])
-//            .sink { photoResponseDTOs in
-//                let photos = photoResponseDTOs.map { $0.toDomain() }
-//                self.photos += photos
-//            }
-//    }
-    
-//    private func fetchTopics() {
-//        let topicRequestDTO = TopicRequestDTO()
-//
-//        guard let urlRequest = API.getListTopics(topicRequestDTO).urlRequest else {
-//            return
-//        }
-//
-//        cancellable = URLSession.shared.dataTaskPublisher(for: urlRequest)
-//            .map { $0.data }
-//            .decode(type: [TopicResponseDTO].self, decoder: JSONDecoder())
-//            .replaceError(with: [])
-//            .sink { topicResponseDTOs in
-//                let topics = topicResponseDTOs.map { $0.toDomain() }
-//                self.topics = topics
-//            }
-//    }
+    private func fetchSearchPhotos() {
+        guard !self.isLoading else {
+            return
+        }
+        
+        self.isLoading = true
+        self.page += 1
+        
+        let photoSearchRequestDTO = PhotoSearchRequestDTO(query: self.searchText, page: self.page)
+        
+        guard let urlRequest = API.getSearchPhotos(photoSearchRequestDTO).urlRequest else {
+            return
+        }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .map { $0.data }
+            .decode(type: SearchResponseDTO.self, decoder: JSONDecoder())
+            .replaceError(with: SearchResponseDTO(results: []))
+            .sink { searchResponseDTO in
+                let photos = searchResponseDTO.results.map { $0.toDomain() }
+                self.photos += photos
+                self.isLoading = false
+            }
+    }
 }
 
 struct SearchView_Previews: PreviewProvider {
